31b155f05ee63ed3ee0a3863f8263bf6524e0249
João Victor <59948734+jv-dev@users.noreply.github.com>
2025-03-16 19:49:17 -0300

    Mensagens de nterações com usuário

--- main.py
+++ main.py
@@ -25,11 +25,20 @@ def handle_questions(gemini_service, messages):
         messages = process_response(gemini_service, messages, user_question)
         counter += 1
 
+    print("Espero que eu tenha ajudado! Até mais.")
 
 def execute_chat_bot():
     gemini_service = GeminiService()
     messages = GeminiService.get_base_context()
+    handle_welcome_message()
     handle_questions(gemini_service, messages)
+
+
+def handle_welcome_message():
+    print("Olá! Bem-vindo ao seu Assistente Financeiro Pessoal.")
+    print("Eu sou um chatbot baseado em IA, projetado para te ajudar com suas finanças.")
+    print("Estou aqui para responder suas perguntas sobre orçamento, investimentos, planejamento financeiro e muito mais!")
+    print("Vamos começar!\n")
 
 
 execute_chat_bot()
